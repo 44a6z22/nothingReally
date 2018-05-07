34d5ceaf65d55581149b16a79dfd0f52aa409118
Hamza <hhamdaoui31@gmail.com>
2018-05-07 21:14:16 +0100

    ...

--- js/main.js
+++ js/main.js
@@ -24,21 +24,19 @@ function change(bool){
 	/**
 	 * [just a wast of time :D]
 	 */
-function onStartup(){
-	
+function onStartup(){	
 var code = new Array("a", "b", "c", "d", "e", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z");
 var Meca = window.prompt("show me what you got ? ","");
 var ThatEntry = Math.round((code.length*54)/48-7+(45*3));
 var yourEntry ;
 var	i = 0;
-for(i = 0 ; i < code.length ; i++){
-	ThatEntry += i;
-}
-
-if(Meca != ThatEntry) {
-	alert("Erreur !!");
-	window.location.href="https://github.com/44a6z22/44a6z22.github.io/blob/master/README.org";
-} else {
-	
-}
+	for(i = 0 ; i < code.length ; i++){
+		ThatEntry += i;
+	}
+	if(Meca != ThatEntry) {
+		alert("Erreur !!");
+		window.location.href="https://github.com/44a6z22/44a6z22.github.io/blob/master/README.org";
+	} else {
+		
+	}
 }
